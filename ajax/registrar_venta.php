cddbf809b7c58071908ba6c4458c37706e5242b3
NwGuille24 <98178981+NwGuille24@users.noreply.github.com>
2022-09-10 08:13:00 -0500

    Update registrar_venta.php

--- ajax/registrar_venta.php
+++ ajax/registrar_venta.php
@@ -5,27 +5,39 @@
 
 
 	$id = $_POST["id_pv"];   
-	$canti = $_POST["cantidad_v"];   	 
-		
-	$sql3="INSERT INTO   ventas	 (id_inventario, catidad_prod) 
-	VALUES ('$id','$canti')";
-	$query_new_insert = mysqli_query($con,$sql3);
+	$canti = $_POST["cantidad_v"];   
 
-	$sql = "SELECT cantidad_stok FROM inventario WHERE id_producto = '$id'";
+
+	$sql = "SELECT cantidad_stok FROM inventario WHERE id_producto = '$id' AND cantidad_stok > '$canti'";
 	$query = mysqli_query($con, $sql);
 	$row = mysqli_fetch_array($query);
-	$cantidad = $row['cantidad_stok'];
+	$cantidad = $row['cantidad_stok'];	 
 
-	$restar = $cantidad - $canti;
+if ($cantidad <= 0) {
+	echo '<script language="javascript">alert("Cantidad no disponible");window.location.href="../productos.php"</script>';
 
-	$sql="UPDATE inventario SET  cantidad_stok = '$restar' WHERE id_producto = $id";
-	$query_update = mysqli_query($con,$sql);
-	
-	$sql3="INSERT INTO   cambios (tipo, id_producto) 
+}
+else{
+    $sql3="INSERT INTO   ventas	 (id_inventario, catidad_prod) 
+	VALUES ('$id','$canti')";
+    $query_new_insert = mysqli_query($con, $sql3);
+
+    $sql = "SELECT cantidad_stok FROM inventario WHERE id_producto = '$id'";
+    $query = mysqli_query($con, $sql);
+    $row = mysqli_fetch_array($query);
+    $cantidad = $row['cantidad_stok'];
+
+    $restar = $cantidad - $canti;
+
+    $sql="UPDATE inventario SET  cantidad_stok = '$restar' WHERE id_producto = $id";
+    $query_update = mysqli_query($con, $sql);
+
+    $sql3="INSERT INTO   cambios (tipo, id_producto) 
 	VALUES ('venta','$id')";
-	$query_new_insert = mysqli_query($con,$sql3);
-	
+    $query_new_insert = mysqli_query($con, $sql3);
 
-	header("Location:../productos.php");
-	exit();
+
+    header("Location:../productos.php");
+    exit();
+}
 ?>
